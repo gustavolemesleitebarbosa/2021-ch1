ac9f6e344e043bf5aabe3bb361d0fb44ad03e8ac
GustavoLemesLB <gustavo.lemes@work.co>
2022-12-25 15:04:16 -0300

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,23 +9,28 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id:  new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    };
+    setTasks([...tasks,newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+   const updatedTasks = tasks.map(task => task.id === id ? { ...task, done : !task.done} : task)
+   setTasks(updatedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const updatedTasks = tasks.filter(task => task.id !== id)
+    setTasks(updatedTasks)
   }
 
   return (
     <View style={styles.container}>
       <Header tasksCounter={tasks.length} />
-
       <TodoInput addTask={handleAddTask} />
-
       <TasksList 
         tasks={tasks} 
         toggleTaskDone={handleToggleTaskDone}
